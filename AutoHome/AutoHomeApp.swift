02cdd16492cef3b8c5c62c558b09598753e99429
sandeep <sandeep@fluxa.io>
2023-08-31 20:47:51 +0530

    FirstCommit

---
+++ AutoHome/AutoHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoHomeApp.swift
+//  AutoHome
+//
+//  Created by Sandeep on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AutoHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
